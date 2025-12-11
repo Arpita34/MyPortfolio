e8b6f62ff22bd4a814092eb2a561e449417b848b
Arpita34 <pathakarpita867@gmail.com>
2025-12-11 23:15:31 +0530

    Updated About section

--- myPortFolio/src/components/About/About.jsx
+++ myPortFolio/src/components/About/About.jsx
@@ -63,7 +63,7 @@ export const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1IWJag3mjRh4aY6zzJVH-g6Xwg3g_X-gh/view?usp=sharing"
+            href="https://drive.google.com/file/d/19rW5la0aUGUPrr8V5-nZAktXZVGOBLhZ/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
